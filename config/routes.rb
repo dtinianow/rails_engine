9aad860511bbfee68798fbe1bb2d218e9e633e52
David Tinianow <david.tinianow@gmail.com>
2016-09-04 18:55:24 -0600

    Merchants controller has all endpoints User can get json data from index, show, find, find_all, and random from merchants controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      get 'merchants/find', to: 'merchants#find'
+      get 'merchants/find_all', to: 'merchants#find_all'
+      resources :merchants, only: [:index, :show]
+    end
+  end
 end
