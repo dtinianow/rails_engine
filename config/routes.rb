f65bfba9846872a737c11e517c622bc56ca05d2e
matthewrpacker <matthewrpacker@gmail.com>
2016-09-04 19:57:07 -0600

    Add invoices record endpoint with appropriate routes - User can search by index, show, find, find_all, and random

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,13 @@ Rails.application.routes.draw do
     namespace :v1 do
       get 'merchants/find', to: 'merchants#find'
       get 'merchants/find_all', to: 'merchants#find_all'
+      get 'merchants/random', to: 'merchants#random'
       resources :merchants, only: [:index, :show]
+
+      get 'invoices/find', to: 'invoices#find'
+      get 'invoices/find_all', to: 'invoices#find_all'
+      get 'invoices/random', to: 'invoices#random'
+      resources :invoices, only: [:index, :show]
     end
   end
 end
