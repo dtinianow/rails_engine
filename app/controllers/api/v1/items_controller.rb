4bacc39e9914f789ce9d1f4c3f6cc244dc8deb0a
David Tinianow <david.tinianow@gmail.com>
2016-09-06 14:17:49 -0600

    Create items find controller

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -5,14 +5,6 @@ class Api::V1::ItemsController < ApplicationController
     respond_with Item.all
   end
 
-  def find
-    respond_with Item.find_by(item_params)
-  end
-
-  def find_all
-    respond_with Item.where(item_params)
-  end
-
   def show
     respond_with Item.find(params[:id])
   end
@@ -21,11 +13,5 @@ class Api::V1::ItemsController < ApplicationController
     random_id = rand(Item.count)
     random_item = Item.find(random_id)
     respond_with random_item
-  end
-
-  private
-
-  def item_params
-    params.permit(:id, :name, :description, :unit_price, :merchant_id, :created_at, :updated_at)
   end
 end
