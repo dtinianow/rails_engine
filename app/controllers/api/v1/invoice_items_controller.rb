788cb553d89fe893a3048a2ecec5b083f6d2f8f6
David Tinianow <david.tinianow@gmail.com>
2016-09-06 14:31:00 -0600

    Create invoice items find controller

--- app/controllers/api/v1/invoice_items_controller.rb
+++ app/controllers/api/v1/invoice_items_controller.rb
@@ -5,14 +5,6 @@ class Api::V1::InvoiceItemsController < ApplicationController
     respond_with InvoiceItem.all
   end
 
-  def find
-    respond_with InvoiceItem.find_by(invoice_item_params)
-  end
-
-  def find_all
-    respond_with InvoiceItem.where(invoice_item_params)
-  end
-
   def show
     respond_with InvoiceItem.find(params[:id])
   end
@@ -21,11 +13,5 @@ class Api::V1::InvoiceItemsController < ApplicationController
     random_id = rand(InvoiceItem.count)
     random_item = InvoiceItem.find(random_id)
     respond_with random_item
-  end
-
-  private
-
-  def invoice_item_params
-    params.permit(:id, :item_id, :invoice_id, :quantity, :unit_price, :created_at, :updated_at)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -20,8 +20,8 @@ Rails.application.routes.draw do
       get 'items/random',   to: 'items/random#show'
       resources :items, only: [:index, :show]
 
-      get 'invoice_items/find',     to: 'invoice_items#find'
-      get 'invoice_items/find_all', to: 'invoice_items#find_all'
+      get 'invoice_items/find_all', to: 'invoice_items/find#index'
+      get 'invoice_items/find',     to: 'invoice_items/find#show'
       get 'invoice_items/random',   to: 'invoice_items#random'
       resources :invoice_items, only: [:index, :show]
     end
