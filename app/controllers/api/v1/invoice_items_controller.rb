0a20bbef5a8160e62dc5e23543f4366f2b5cca46
matthewrpacker <matthewrpacker@gmail.com>
2016-09-06 10:36:33 -0600

    Setup record endpoint for invoice items with routes

---
+++ app/controllers/api/v1/invoice_items_controller.rb
@@ -0,0 +1,31 @@
+class Api::V1::InvoiceItemsController < ApplicationController
+  respond_to :json
+
+  def index
+    respond_with InvoiceItem.all
+  end
+
+  def find
+    respond_with InvoiceItem.find_by(invoice_item_params)
+  end
+
+  def find_all
+    respond_with InvoiceItem.where(invoice_item_params)
+  end
+
+  def show
+    respond_with InvoiceItem.find(params[:id])
+  end
+
+  def random
+    random_id = rand(InvoiceItem.count)
+    random_item = InvoiceItem.find(random_id)
+    respond_with random_item
+  end
+
+  private
+
+  def invoice_item_params
+    params.permit(:id, :item_id, :invoice_id, :quantity, :unit_price, :created_at, :updated_at)
+  end
+end
