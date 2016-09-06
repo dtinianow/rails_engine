42ab3e6b058a61d59796f4a2f8e2a85752764c05
David Tinianow <david.tinianow@gmail.com>
2016-09-06 14:33:21 -0600

    Create invoice items random controller

--- app/controllers/api/v1/invoice_items_controller.rb
+++ app/controllers/api/v1/invoice_items_controller.rb
@@ -8,10 +8,4 @@ class Api::V1::InvoiceItemsController < ApplicationController
   def show
     respond_with InvoiceItem.find(params[:id])
   end
-
-  def random
-    random_id = rand(InvoiceItem.count)
-    random_item = InvoiceItem.find(random_id)
-    respond_with random_item
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
 
       get 'invoice_items/find_all', to: 'invoice_items/find#index'
       get 'invoice_items/find',     to: 'invoice_items/find#show'
-      get 'invoice_items/random',   to: 'invoice_items#random'
+      get 'invoice_items/random',   to: 'invoice_items/random#show'
       resources :invoice_items, only: [:index, :show]
     end
   end
