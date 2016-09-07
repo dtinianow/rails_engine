297dd5428414ef457e5bb8a8941cf55eb62523e0
David Tinianow <david.tinianow@gmail.com>
2016-09-07 13:52:54 -0600

    Create item ranked by revenue method

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,12 @@
 class Item < ApplicationRecord
   belongs_to :merchant
-
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
-end
+
+  def self.ranked_by_revenue(quantity)
+    joins(:invoice_items, invoices: :transactions).
+    where(transactions: {result:'success'}).
+    group(:id).
+    order('sum(invoice_items.quantity*invoice_items.unit_price) DESC').
+    take(1)
+  end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,5 +1,4 @@
 class Merchant < ApplicationRecord
-
   has_many :invoices
   has_many :items
   has_many :customers, through: :invoices
