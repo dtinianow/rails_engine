d0276e0272a7a9cac467ba356a437346fe4aa5a2
David Tinianow <david.tinianow@gmail.com>
2016-09-07 13:22:14 -0600

    Close #41: Favorite merchants endpoint (#42)
    * Create favorite merchants method in customer model
    
    * Create controller and route for customer favorite merchant endpoint

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,15 @@
 class Customer < ApplicationRecord
   has_many :invoices
+  has_many :merchants, through: :invoices
   has_many :transactions, through: :invoices
+
+  def self.favorite_merchant(customer_id)
+    find(customer_id).
+    merchants.
+    joins(invoices: :transactions).
+    where(transactions: {result: 'success'}).
+    group(:id).
+    order('count(invoices.merchant_id) DESC').
+    first
+  end
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 RSpec.describe Customer, type: :model do
   it { should have_many :invoices }
   it { should have_many(:transactions).through(:invoices) }
+  it { should have_many(:merchants).through(:invoices)}
 end
