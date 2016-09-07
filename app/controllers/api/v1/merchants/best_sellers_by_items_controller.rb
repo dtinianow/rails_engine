2b04e79b328224f6e756542454914dc72131cc17
David Tinianow <david.tinianow@gmail.com>
2016-09-07 16:13:44 -0600

    Fix syntax

--- app/controllers/api/v1/merchants/best_sellers_by_items_controller.rb
+++ app/controllers/api/v1/merchants/best_sellers_by_items_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::Merchants::BestSellersByItemsController < ApplicationController
   respond_to :json
 
   def index
-
     respond_with Merchant.ranked_by_items_sold(params[:quantity])
   end
 end
