ffaea79579a68657aa7557571d988379e6c1a5c9
David Tinianow <david.tinianow@gmail.com>
2016-09-08 16:27:37 -0600

    Complete random controller for customers and transactions

---
+++ spec/requests/api/v1/transactions/random_controller_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe "Transactions Random" do
+  fixtures :transactions
+
+  context '#Show' do
+
+    it 'returns JSON data on random transactions' do
+      get '/api/v1/transactions/random'
+
+      transaction = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(transaction.class).to eq(Hash)
+      expect(transaction).to have_key('id')
+      expect(transaction).to have_key('invoice_id')
+      expect(transaction).to have_key('credit_card_number')
+      expect(transaction).to have_key('result')
+    end
+  end
+end
