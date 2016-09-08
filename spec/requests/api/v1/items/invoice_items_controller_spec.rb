439e8abeb00ab0a206714a6d9c27550d814067db
Matt Packer <matthewrpacker@gmail.com>
2016-09-08 17:16:11 -0600

    Items controller specs Closes #76, Closes #77, Closes #78, Closes #79 (#92)
    * Create items merchants controller spec
    
    * Create items invoice items controller spec
    
    * Create items best sellers by revenue controller spec
    
    * Create items best sellers by items controller spec

---
+++ spec/requests/api/v1/items/invoice_items_controller_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe "Item's Invoice Items Index" do
+  fixtures :items
+  fixtures :invoice_items
+
+  context "#Index" do
+
+    it 'returns JSON data on all invoice items matching item id params' do
+      get '/api/v1/items/2/invoice_items'
+
+      invoice_items = JSON.parse(response.body)
+      invoice_item = invoice_items.first
+
+      expect(response).to be_success
+      expect(invoice_items.length).to eq(1)
+      expect(invoice_items.class).to eq(Array)
+
+      expect(invoice_item.class).to eq(Hash)
+      expect(invoice_item['id']).to eq(2)
+      expect(invoice_item['item_id']).to eq(2)
+      expect(invoice_item['invoice_id']).to eq(2)
+      expect(invoice_item['quantity']).to eq(10)
+      expect(invoice_item['unit_price']).to eq('2000.20')
+    end
+  end
+end
