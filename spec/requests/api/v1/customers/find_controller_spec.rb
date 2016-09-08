243e88faad3805cd0e952471fe1d6a7f7d1fb8b2
matthewrpacker <matthewrpacker@gmail.com>
2016-09-07 20:37:35 -0600

    Revise yaml file structure

--- spec/requests/api/v1/customers/find_controller_spec.rb
+++ spec/requests/api/v1/customers/find_controller_spec.rb
@@ -7,34 +7,32 @@ describe "Customers" do
     get '/api/v1/customers/find_all?first_name=Cecelia'
 
     customers = JSON.parse(response.body)
-    customer1 = customers.first
-    customer2 = customers.last
+    customer = customers.first
 
     expect(response).to be_success
-    expect(customers.length).to eq(2)
-    expect(customer1['id']).to eq(2)
-    expect(customer1['first_name']).to eq('Cecelia')
-    expect(customer1['last_name']).to eq('Osinski')
-    expect(customer2['id']).to eq(3)
-    expect(customer2['first_name']).to eq('Cecelia')
-    expect(customer2['last_name']).to eq('Ondricka')
+    expect(customers.length).to eq(1)
+    expect(customers.class).to eq(Array)
+
+    expect(customer.class).to eq(Hash)
+    expect(customer['id']).to eq(2)
+    expect(customer['first_name']).to eq('Cecelia')
+    expect(customer['last_name']).to eq('Osinski')
   end
 
   it 'returns JSON data on all customers matching last_name params' do
     get '/api/v1/customers/find_all?last_name=Ondricka'
 
     customers = JSON.parse(response.body)
-    customer1 = customers.first
-    customer2 = customers.last
+    customer = customers.first
 
     expect(response).to be_success
-    expect(customers.length).to eq(2)
-    expect(customer1['id']).to eq(1)
-    expect(customer1['first_name']).to eq('Joey')
-    expect(customer1['last_name']).to eq('Ondricka')
-    expect(customer2['id']).to eq(3)
-    expect(customer2['first_name']).to eq('Cecelia')
-    expect(customer2['last_name']).to eq('Ondricka')
+    expect(customers.length).to eq(1)
+    expect(customers.class).to eq(Array)
+
+    expect(customer.class).to eq(Hash)
+    expect(customer['id']).to eq(1)
+    expect(customer['first_name']).to eq('Joey')
+    expect(customer['last_name']).to eq('Ondricka')
   end
 
   it 'returns JSON data on a specific customer' do
@@ -42,6 +40,7 @@ describe "Customers" do
 
     customer = JSON.parse(response.body)
 
+    expect(customer.class).to eq(Hash)
     expect(response).to be_success
     expect(customer['id']).to eq(2)
     expect(customer['first_name']).to eq('Cecelia')
@@ -53,6 +52,7 @@ describe "Customers" do
 
     customer = JSON.parse(response.body)
 
+    expect(customer.class).to eq(Hash)
     expect(response).to be_success
     expect(customer['id']).to eq(1)
     expect(customer['first_name']).to eq('Joey')
--- spec/requests/api/v1/items_controller_spec.rb
+++ spec/requests/api/v1/items_controller_spec.rb
@@ -11,10 +11,13 @@ describe "Items" do
 
     expect(response).to be_success
     expect(items.length).to eq(2)
+    expect(items.class).to eq(Array)
+
+    expect(item.class).to eq(Hash)
     expect(item['id']).to eq(1)
     expect(item['name']).to eq("Golf Club")
     expect(item['description']).to eq("Intended to strike golf balls.")
-    expect(item['unit_price']).to eq(75107)
+    expect(item['unit_price']).to eq(10001)
     expect(item['merchant_id']).to eq(1)
   end
 
@@ -24,10 +27,11 @@ describe "Items" do
     item = JSON.parse(response.body)
 
     expect(response).to be_success
+    expect(item.class).to eq(Hash)
     expect(item['id']).to eq(2)
     expect(item['name']).to eq("Golf Ball")
     expect(item['description']).to eq("Intended to be striken by a golf club.")
-    expect(item['unit_price']).to eq(67076)
-    expect(item['merchant_id']).to eq(1)
+    expect(item['unit_price']).to eq(20002)
+    expect(item['merchant_id']).to eq(2)
   end
 end
