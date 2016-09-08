a087656a78baf0930e3dbc4e449ee5ff50ba5d9b
David Tinianow <david.tinianow@gmail.com>
2016-09-08 16:51:49 -0600

    Complete items random controller spec

--- app/controllers/api/v1/items/random_controller.rb
+++ app/controllers/api/v1/items/random_controller.rb
@@ -4,5 +4,6 @@ class Api::V1::Items::RandomController < ApplicationController
   def show
     random_offset = rand(Item.count)
     random_item = Item.offset(random_offset).first
-    respond_with random_item  end
+    respond_with random_item
+  end
 end
