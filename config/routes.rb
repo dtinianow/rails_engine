c308b6a090d0c3e2b16cc787407515b6c0585e92
matthewrpacker <matthewrpacker@gmail.com>
2016-09-08 09:29:02 -0600

    Refactor namespacing in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+      resources :merchants, only: [:index, :show]
+      resources :invoices, only: [:index, :show]
+      resources :items, only: [:index, :show]
+      resources :invoice_items, only: [:index, :show]
+      resources :customers, only: [:index, :show]
+      resources :transactions, only: [:index, :show]
+      
       namespace :merchants do
         get 'find_all',              to: 'find#index'
         get 'find',                  to: 'find#show'
@@ -14,7 +21,6 @@ Rails.application.routes.draw do
         get ':id/invoices',          to: 'invoices#index'
         get ':id/customers_with_pending_invoices', to: 'pending_invoices#index'
       end
-      resources :merchants, only: [:index, :show]
 
       namespace :invoices do
         get 'find_all',          to: 'find#index'
@@ -26,7 +32,6 @@ Rails.application.routes.draw do
         get ':id/customer',      to: 'customers#show'
         get ':id/merchant',      to: 'merchants#show'
       end
-      resources :invoices, only: [:index, :show]
 
       namespace :items do
         get 'find_all',          to: 'find#index'
@@ -37,7 +42,6 @@ Rails.application.routes.draw do
         get 'most_revenue',      to: 'best_sellers_by_revenue#index'
         get 'most_items',        to: 'best_sellers_by_items#index'
       end
-      resources :items, only: [:index, :show]
 
       namespace :invoice_items do
         get 'find_all',    to: 'find#index'
@@ -46,7 +50,6 @@ Rails.application.routes.draw do
         get ':id/invoice', to: 'invoices#show'
         get ':id/item',    to: 'items#show'
       end
-      resources :invoice_items, only: [:index, :show]
 
       namespace :customers do
         get 'find_all',              to: 'find#index'
@@ -56,7 +59,6 @@ Rails.application.routes.draw do
         get ':id/transactions',      to: 'transactions#index'
         get ':id/favorite_merchant', to: 'merchants#show'
       end
-      resources :customers, only: [:index, :show]
 
       namespace :transactions do
         get 'find_all',    to: 'find#index'
@@ -64,7 +66,6 @@ Rails.application.routes.draw do
         get 'random',      to: 'random#show'
         get ':id/invoice', to: 'invoices#show'
       end
-      resources :transactions, only: [:index, :show]
     end
   end
 end
