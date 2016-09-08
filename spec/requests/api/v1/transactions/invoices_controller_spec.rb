bb0212cfb0696db6dea52288279c62649e4a1aeb
David Tinianow <david.tinianow@gmail.com>
2016-09-08 17:13:04 -0600

    Complete spec for transactions invoices controller

---
+++ spec/requests/api/v1/transactions/invoices_controller_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe "Transactions Invoices" do
+  fixtures :transactions
+  fixtures :invoices
+
+  context "#Show" do
+
+    it 'returns JSON data on an invoice belonging to a specific transaction' do
+      get '/api/v1/transactions/1/invoice'
+
+      invoice = JSON.parse(response.body)
+
+      expect(invoice.class).to eq(Hash)
+      expect(response).to be_success
+      expect(invoice['id']).to eq(1)
+      expect(invoice['customer_id']).to eq(1)
+      expect(invoice['merchant_id']).to eq(1)
+      expect(invoice['status']).to eq('shipped')
+    end
+  end
+end
