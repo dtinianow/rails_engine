aeb08a04ee4a2936fe005d505995b0610262f05d
matthewrpacker <matthewrpacker@gmail.com>
2016-09-08 18:26:53 -0600

    Create merchants best sellers by revenue controller spec

--- spec/requests/api/v1/merchants/best_sellers_by_revenue_controller_spec.rb
+++ spec/requests/api/v1/merchants/best_sellers_by_revenue_controller_spec.rb
@@ -1,15 +1,14 @@
 require 'rails_helper'
 
-describe "Merchants best sellers by items" do
+describe "Merchants best sellers by revenue" do
   fixtures :merchants
-  fixtures :items
   fixtures :invoices
   fixtures :transactions
 
   context "#Index" do
 
-    it 'returns JSON data on all merchants by items sold matching quantity params' do
-      get '/api/v1/merchants/most_items?quantity=1'
+    it 'returns JSON data on all best merchant sellers by revenue matching quantity params' do
+      get '/api/v1/merchants/most_revenue?quantity=1'
 
       best_sellers = JSON.parse(response.body)
       best_seller = best_sellers.first
@@ -17,6 +16,7 @@ describe "Merchants best sellers by items" do
       expect(response).to be_success
       expect(best_sellers.length).to eq(1)
       expect(best_sellers.class).to eq(Array)
+
       expect(best_seller.class).to eq(Hash)
       expect(best_seller['id']).to eq(1)
       expect(best_seller['name']).to eq("David's Store")
